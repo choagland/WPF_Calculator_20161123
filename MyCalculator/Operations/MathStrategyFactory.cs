bd7731a1a2056443dae3b1d9f48ccd108b2540c1
Clint Hoagland <c.hoagland@techsmith.com>
2016-12-07 11:15:18 -0500

    + MathStrategyFactory

---
+++ MyCalculator/Operations/MathStrategyFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyCalculator.Operations
+{
+   public class MathStrategyFactory
+   {
+      public IMathStrategy SelectMathStrategy( Operation operation )
+      {
+         switch ( operation )
+         {
+            case Operation.InitialState:
+            {
+               return new InitialMathStrategy();
+            }
+            case Operation.Add:
+            {
+               return new AdditionStrategy();
+            }
+            default:
+            {
+               throw new NotImplementedException();
+            }
+         }
+      }
+   }
+
+   public class InitialMathStrategy : IMathStrategy
+   {
+      public double Calculate( double x, double y )
+      {
+         return y;
+      }
+   }
+
+   public class AdditionStrategy: IMathStrategy
+   {
+      public double Calculate( double x, double y )
+      {
+         return x + y;
+      }
+   }
+}
